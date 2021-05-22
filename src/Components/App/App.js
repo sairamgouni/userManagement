f034f1a4b157144f1f0f90395b6c1f96f4e228ba
sairamgouni <gounisairam@gmail.com>
2021-05-23 00:18:25 +0800

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+
+function App() {
+    return (
+        <></>
+    );
+}
+
+export default App;
